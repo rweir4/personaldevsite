7db42b70e49a5772871204faf1d744abbe65fad0
rweir4 <rebeccaweir12@gmail.com>
2023-06-02 14:13:51 -0400

    Replace apostraphe

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,27 @@
-import "./globals.css"
-import styles from "./page.module.scss"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import './globals.css'
+import styles from './page.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export const metadata = {
-  title: "Rebecca Leigh Weir",
-  description: "Personal Developer Site",
+  title: 'Rebecca Leigh Weir',
+  description: 'Personal Developer Site',
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         <nav>
           <niv className={styles.menu}>
-            <a href="/">Home</a>
-            <a href="/projects">Projects</a>
-            <a href="/about">About</a>
+            <a href='/'>Home</a>
+            <a href='/projects'>Projects</a>
+            <a href='/about'>About</a>
           </niv>
         </nav>
         {children}
         <footer className={styles.footer}>
-          <a href="https://www.linkedin.com/in/rebeccalweir/" className={styles.socialLinks}>
+          <a href='https://www.linkedin.com/in/rebeccalweir/' className={styles.socialLinks}>
             <FontAwesomeIcon icon={faLinkedin} className={styles.socials}/>
             <p>LinkedIn</p>
           </a>
--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import styles from "./page.module.scss";
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
@@ -6,11 +6,11 @@ export default function Home() {
       <div className={styles.intro}>
         <h1 className={styles.title}>Rebecca Leigh Weir</h1>
         <h2 className={styles.subtitle}>FullStack Developer</h2>
-        <p className={styles.description}>As a coder and a creative writer, I believe that storytelling is capable of making a difference, and software development is another way to tell a story, whether it be the code"s, the company"s, or the customer"s.</p>
+        <p className={styles.description}>As a coder and a creative writer, I believe that storytelling is capable of making a difference, and software development is another way to tell a story, whether it be the code&apos;s, the company&apos;s, or the customer&apos;s.</p>
       </div>
       <img 
-        src="/rebecca-outline.png" 
-        alt="Picture of Developer" 
+        src='/rebecca-outline.png' 
+        alt='Picture of Developer' 
         className={styles.picture} />
     </main>
   )
