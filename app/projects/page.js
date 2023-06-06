a3bc7a75bd0f90fb634a11448c3e82bc6dd64047
rweir4 <rebeccaweir12@gmail.com>
2023-06-06 15:23:09 -0400

    Update projects to include professional

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,10 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import styles from 'app/page.module.scss'
 
 export default function Projects() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.page_titles}>Projects</h1>
-      <div className={styles.projects}>
+      <section>
+        <h2>Professional</h2>
+        <a href='#professional' className={styles.arrow_icon}>
+          <FontAwesomeIcon icon={faAngleDown} />
+        </a>
+      </section>
+      <section id='professional' className={styles.professional}>
+        <div>
+          <h3>Vimeo, Inc.</h3>
+          <img src='vimeo.jpeg' />
+        </div>
+        <div>
+          <h3>Medidata Solutions, <br />A Dassault Systemes Company</h3>
+          <img src='/medidata.jpeg' />
+        </div>
+      </section>
+      <section>
+        <h2>Personal</h2>
+        <a href='#personal' className={styles.arrow_icon}>
+          <FontAwesomeIcon icon={faAngleDown} />
+        </a>
+      </section>
+      <section id='personal' className={styles.projects}>
         <div>
           <a href='projects/developer_porfolio'>
             <img src='/keyboard.jpg' alt='computer' width='250' height='350' 
@@ -26,7 +49,7 @@ export default function Projects() {
             <h3 className={styles.project_title}>Park Features</h3>
           </a>
         </div>
-      </div>
+      </section>
     </main>
   )
 }
