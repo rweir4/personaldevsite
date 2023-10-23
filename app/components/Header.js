b5db409a57c03acf90a5d05e3b84e24f329aa751
rweir4 <rebeccaweir12@gmail.com>
2023-10-23 11:18:36 -0400

    Add toggle function to submenu

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,9 +7,14 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
   let [hamburgerOpen, setHamburgerOpen] = useState(false);
+  let [subNav, setSubNavOpen] = useState(false);
 
   const toggleHamburger = () => {
     setHamburgerOpen(!hamburgerOpen);
+  };
+  
+  const toggleSubNav = () => {
+    setSubNavOpen(!subNav);
   };
 
   return (
@@ -19,14 +24,14 @@ export default function Header() {
       </a>
       <nav className={styles.navbar}>
         <a href='/'>Home</a>
-        <div className={styles.subnav}>
+        <div className={styles.subnav} onClick={toggleSubNav}>
           <a className={styles.subnavbtn}>Development</a>
-          <div className={styles.subnav_content}>
+          {subNav && <div className={styles.subnav_content}>
             <a href="/development/fullstack_engineer">Full Stack Engineering</a>
             <a href="/development/game_development">Game Development</a>
             <a href="/development/web_design_and_development">Web Design & Development</a>
             <a href="/development/software_projects">Software Projects</a>
-          </div>
+          </div>}
         </div>
         <a href='/books'>Books</a>
         <a href='/about'>About</a>
@@ -36,14 +41,14 @@ export default function Header() {
         onClick={toggleHamburger}/>
       {hamburgerOpen && <nav className={styles.navbar_mobile}>
         <a href='/'>Home</a>
-        <div className={styles.subnav}>
+        <div className={styles.subnav} onClick={toggleSubNav}>
           <a className={styles.subnavbtn}>Development</a>
-          <div className={styles.subnav_content}>
+          {subNav && <div className={styles.subnav_content}>
             <a href="/development/fullstack_engineer">Full Stack Engineering</a>
             <a href="/development/game_development">Game Development</a>
             <a href="/development/web_design_and_development">Web Design & Development</a>
             <a href="/development/software_projects">Software Projects</a>
-          </div>
+          </div>}
         </div>
         <a href='/books'>Books</a>
         <a href='/about'>About</a>
