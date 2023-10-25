309764f0d7d48946ba77fb5e615e116d6335d73a
rweir4 <rebeccaweir12@gmail.com>
2023-10-25 14:08:37 -0400

    Escape apostrophe

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,7 +4,7 @@ export default function WorkWithMe() {
   return (
     <main className={styles.main}>
       <img src='/lets-talk.png' className={styles.banner} />
-      <h2>I'd love to hear from you!</h2>
+      <h2>I&apos;d love to hear from you!</h2>
       <h3 className={styles.email}>
         If you are interested in working with me in any capacity, 
         please email me at rebeccaweir12@gmail.com.
