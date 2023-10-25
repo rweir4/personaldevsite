0a6655a6251fb31286ca0d12139387e21e4bb572
rweir4 <rebeccaweir12@gmail.com>
2023-10-25 13:54:16 -0400

    Update home to deal with other updates

--- app/page.js
+++ app/page.js
@@ -8,14 +8,16 @@ export default function Home() {
         <img
           src='rose-paint.png'
           className={styles.rose_background} />
-        <h1 className={styles.title}>Rebecca Leigh Weir</h1>
-        <h2 className={styles.subtitle}>Full Stack Software Engineer, Web Designer, and YA Author</h2>
-        <p className={styles.description}>As a coder and a creative writer, I believe that storytelling is capable of making a difference, and software development is another way to tell a story, whether it be the code&apos;s, the company&apos;s, or the customer&apos;s.</p>
+        <div className={styles.intro_info}>
+          <h1 className={styles.title}>Rebecca Leigh Weir</h1>
+          <h2 className={styles.subtitle}>Full Stack Software Engineer, Web Designer, and YA Author</h2>
+          <p className={styles.description}>As a coder and a creative writer, I believe that storytelling is capable of making a difference, and software development is another way to tell a story, whether it be the code&apos;s, the company&apos;s, or the customer&apos;s.</p>
+        </div>
       </div>
       <img 
         src='/rebecca-outline-eggplant.png' 
         alt='Picture of Developer' 
-        className={styles.picture} />
+        className={styles.outline_picture} />
     </main>
   )
 }
