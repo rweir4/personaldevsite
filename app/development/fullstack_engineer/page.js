454f39a387900a55872104c4faa708e27a7eff67
rweir4 <rebeccaweir12@gmail.com>
2023-10-25 14:44:06 -0400

    Redo fullstack page

--- app/development/fullstack_engineer/page.js
+++ app/development/fullstack_engineer/page.js
@@ -6,20 +6,21 @@ export default function FullstackEngineer() {
   return (
     <main className={styles.main}>
       <div className={styles.professional}>
-        <div className={styles.logo_left} />
-        <img src='/medidata.webp' alt='medidata' />
-        <div className={styles.professional_title}>
+        <img src='/fullstack-banner.png' className={styles.professional_banner}/>
+        <div className={styles.workplaces}>
+          <img src='/medidata.webp' alt='medidata'/>
+          <img src='/vimeo.png' alt='vimeo'/>
+        </div>
+      </div>
+      <div className={styles.professional_buttons}>
+        <div>
           <h3>Medidata Solutions, A Dassault Systemes Company</h3>
           <a href='/development/medidata' className={styles.secondary_btn}>See More</a>
         </div>
-      </div>
-      <div className={styles.professional}>
-        <div className={styles.professional_title}>
+        <div>
           <h3>Vimeo, Inc.</h3>
           <a href='/development/vimeo' className={styles.secondary_btn}>See More</a>
         </div>
-        <div className={styles.logo_right} />
-        <img src='/vimeo.png' alt='vimeo' />
       </div>
     </main>
   )
