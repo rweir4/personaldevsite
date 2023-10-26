56c8754037c5020fab6161bf6ccea372ba0701ea
rweir4 <rebeccaweir12@gmail.com>
2023-10-26 09:40:22 -0400

    Fix software projects styling, web and mobile

--- app/development/software_projects/page.js
+++ app/development/software_projects/page.js
@@ -1,40 +1,37 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import styles from 'app/page.module.scss'
 
 export default function SoftwareProjects() {
   return (
     <main className={styles.main}>
-      <div className={styles.professional}>
-        <div className={styles.logo_left} />
-        <div className={styles.professional_title}>
-          <h3>Korean Translator</h3>
-          <a href='https://github.com/rweir4/korean_translator'alt='github-repo'target="_blank" className={styles.secondary_btn}>
-            Github Repo
-          </a>
+      <div className={styles.software_projects}>
+        <div className={styles.projects}>
+          <div className={styles.projects_info}>
+            <h2>Korean Translator</h2>
+            <a href='https://github.com/rweir4/korean_translator'alt='github-repo'target="_blank" className={styles.secondary_btn}>
+              Github Repo
+            </a>
+          </div>
+          <img src='/korean_food.jpg' className={styles.projects_pic}/>
         </div>
-        <img src='/korean_food.jpg' />
-      </div>
-      <div className={styles.professional}>       
-        <img src='/keyboard.jpg' />
-        <div className={styles.professional_title}>
-          <h3>Developer Portfolio</h3>
-          <a href='https://github.com/rweir4/personaldevsite'alt='github-repo'target="_blank" className={styles.secondary_btn}>
-            Github Repo
-          </a>
+        <div className={styles.projects}>       
+          <img src='/keyboard.jpg' className={styles.projects_pic}/>
+          <div className={styles.projects_info}>
+            <h2>Developer Portfolio</h2>
+            <a href='https://github.com/rweir4/personaldevsite'alt='github-repo'target="_blank" className={styles.secondary_btn}>
+              Github Repo
+            </a>
+          </div>
+        </div>      
+        <div className={styles.projects}>
+          <div className={styles.projects_info}>
+            <h2>Ginny & Co Brand Shop</h2>
+            <a href='https://github.com/rweir4/doganddog' alt='github-repo' target="_blank" className={styles.secondary_btn}>
+              Github Repo
+            </a>
+            <a href='https://ginnyandco.herokuapp.com/' className={styles.secondary_btn}>View Ginny and Co</a>
+          </div>
+          <img src='/ginnyandco.jpg' className={styles.projects_pic}/>
         </div>
-        <div className={styles.logo_right} />
-      </div>      
-      <div className={styles.professional}>
-        <div className={styles.logo_left} />
-        <div className={styles.professional_title}>
-          <h3>Ginny & Co Brand Shop</h3>
-          <a href='https://github.com/rweir4/doganddog' alt='github-repo' target="_blank" className={styles.secondary_btn}>
-            Github Repo
-          </a>
-          <a href='https://ginnyandco.herokuapp.com/' className={styles.secondary_btn}>View Ginny and Co</a>
-        </div>
-        <img src='/ginnyandco.jpg' />
       </div>
     </main>
   )
