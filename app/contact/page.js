8b50e941f4a5dd157973611bcee846b2a1c1f80d
rweir4 <rebeccaweir12@gmail.com>
2023-10-26 10:34:02 -0400

    Fix contact mobile'

--- app/contact/page.js
+++ app/contact/page.js
@@ -5,10 +5,9 @@ export default function WorkWithMe() {
     <main className={styles.main}>
       <img src='/lets-talk.png' className={styles.banner} />
       <h2>I&apos;d love to hear from you!</h2>
-      <h3 className={styles.email}>
-        If you are interested in working with me in any capacity, 
-        please email me at rebeccaweir12@gmail.com.
-      </h3>
+      <p className={styles.email}>
+        If you are interested in working with me in any capacity, please email me at rebeccaweir12@gmail.com
+      </p>
     </main>
   )
 }
